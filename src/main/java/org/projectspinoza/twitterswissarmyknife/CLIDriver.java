94166291ef321cd2d0d2dfeab58266cdf126f15b
datascientistbyday <datascientistbyday@gmail.com>
2015-07-10 21:59:45 -0700

    initial commit

---
+++ src/main/java/org/projectspinoza/twitterswissarmyknife/CLIDriver.java
@@ -0,0 +1,7 @@
+package org.projectspinoza.twitterswissarmyknife;
+
+import com.beust.jcommander.Parameter;
+
+public class CLIDriver {	 
+
+}
---
+++ src/main/java/org/projectspinoza/twitterswissarmyknife/Main.java
@@ -0,0 +1,12 @@
+package org.projectspinoza.twitterswissarmyknife;
+
+import org.json.JSONObject;
+
+public class Main {
+	
+	public static void main (String args[]) {
+		System.out.println("Hello World");
+		JSONObject jo=new JSONObject();
+	}
+
+}
